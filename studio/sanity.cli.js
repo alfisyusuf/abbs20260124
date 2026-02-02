1d2fc91f2ca787dd25ea4fd487c5b8ee65f106c7
alfisyusuf <alfisyusuf@gmail.com>
2026-02-02 21:32:01 +0700

    update

--- studio/sanity.cli.js
+++ studio/sanity.cli.js
@@ -6,10 +6,8 @@ export default defineCliConfig({
     dataset: 'production'
   },
   deployment: {
-    // Tambahkan ID ini (dari terminal Bapak tadi):
     appId: 'm19vj9rjtbsega8486g0hx72',
     
-    // Biarkan fitur update otomatis tetap nyala
     autoUpdates: true, 
   }
 })
